027472abb8a43456eac92e1c7b47ffcaa7d3e954
SeungWoo3 <shtmddn49@kookmin.ac.kr>
2025-10-22 15:23:38 +0900

    component remeasured

--- darknetz/log_test.sh
+++ darknetz/log_test.sh
@@ -10,20 +10,20 @@ CMD="sudo -S host/optee_example_darknetp classifier predict \
 #nmcli radio wifi off
 #sleep 60s
 #nmcli radio wifi off
-# sleep 20s
+sleep 20s
 
-echo "nvidia" | $CMD -num_exp 1
+echo "nvidia" | $CMD -num_exp 100
 
 # echo "nvidia" | $CMD -pp_start 2 -pp_end 10 -num_exp 1
 
 for start in {0..10}; do
  for end in $(seq "$start" 10); do
-   echo "nvidia" | $CMD -pp_start "$start" -pp_end "$end" -num_exp 1
+   echo "nvidia" | $CMD -pp_start "$start" -pp_end "$end" -num_exp 100
    # echo "$start $end"
  done
 done
 
-# poweroff
+poweroff
 
 
 # nmcli radio wifi on
